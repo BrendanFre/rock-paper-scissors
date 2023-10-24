766233d00be5eb359cd58bcb76cd2554bd21cd4d
Brendan Freeman <bfreeman0@gmail.com>
2023-10-24 23:46:00 +0100

    Finished writing the logic for the rock paper scissors game. Next steps are to improve the UI.

--- script.js
+++ script.js
@@ -1,53 +1,95 @@
 let weaponSelection = ["Rock", "Paper", 'Scissors'];
+const rockButton = document.querySelector('#rock')
+const paperButton = document.querySelector('#paper')
+const scissorsButton = document.querySelector('#scissors')
+const gameScreen = document.querySelector('#gameScreen')
+const gameBoard = document.querySelector('#gameBoard')
+let totalPlayerScore = 0
+let totalComputerScore = 0
 
-function getComputerChoice() {
+function getComputerChoice(playerChoice) {
     let randomInt = Math.floor(Math.random() * 3)
-    return weaponSelection[randomInt]
+    let computerChoice
+    if(randomInt == 0){
+        computerChoice = 'rock'
+    } else if(randomInt == 1){
+        computerChoice = 'paper'
+    } else{
+        computerChoice = 'scissors'
+    }
+    const computerMove = document.createElement('li')
+
+    let computerMoveString = `Computer chose ${computerChoice}`
+    computerMove.textContent = computerMoveString
+    computerMove.classList.add('computerTurn')
+
+    gameBoard.appendChild(computerMove)
+    console.log(playerChoice + " " + computerChoice)
+    gameRound(playerChoice, computerChoice)
 };
 
-console.log(getComputerChoice());
 
-function getPlayerChoice(){
-    let playerPrompt = prompt("Please choose either rock, paper or scissors");
-    let lowerCasePlayer = playerPrompt.toLowerCase()
-    if(lowerCasePlayer == "rock") {
-        return weaponSelection[0]
-    } else if (lowerCasePlayer == "paper"){
-        return weaponSelection[1]
-    } else if(lowerCasePlayer == "scissors")
-    {return weaponSelection[2]} 
-    else{
-        console.log("Undefined")
+rockButton.addEventListener('click', getPlayerChoice)
+paperButton.addEventListener('click', getPlayerChoice)
+scissorsButton.addEventListener('click', getPlayerChoice)
+
+function getPlayerChoice(playerSelection) {
+    let playerSelectionString = (playerSelection.target.value).toString()
+    const playerMove = document.createElement('li')
+
+    let playerMoveString = `You chose ${playerSelectionString}`
+    playerMove.textContent = playerMoveString
+    playerMove.classList.add('playerTurn')
+
+    gameBoard.appendChild(playerMove)
+    getComputerChoice(playerSelectionString)
+
+
+}
+
+function gameRound(playerChoice, computerChoice) {
+    let playerScore = 0
+    console.log(playerChoice + " " + computerChoice)
+    if (playerChoice == computerChoice) {
+        playerScore = 'draw'
+    } else if (playerChoice == "rock" && computerChoice == "scissors") {
+        playerScore = 'win'
+    } else if (playerChoice == "scissors" && computerChoice == "paper") {
+        playerScore = 'win'
+    } else if (playerChoice == "paper" && computerChoice == "scissors") {
+        playerScore = 'win'
+    } else {
+        playerScore = 'lose'
+    };
+    console.log('current score to add ' + playerScore)
+    updateTotalScores(playerScore)
+}
+
+function updateTotalScores(playerScore) {
+    if (playerScore == 'lose'){
+        totalComputerScore += 1
+    } else if(playerScore == 'win'){
+        totalPlayerScore += 1
+    }
+    checkScore()
+
+    console.log(totalComputerScore + " " + totalPlayerScore)
+}
+
+function checkScore(){
+    if(totalPlayerScore == 5){
+        gameOver('player')
+    } else if(totalComputerScore == 5){
+        gameOver('computer')
     }
 }
 
-function gameRound(playerChoice, computerSelection){
-    if (playerChoice == computerSelection){
-        return 1
-    } else if (playerChoice == "rock" && computerSelection == "scissors"){
-        return 1
-    } else if (playerChoice == "scissors" && computerSelection == "paper"){
-        return 1
-    } else if (playerChoice == "paper" && computerSelection == "scissors") {
-        return 1
+function gameOver(winner){
+    if(winner == 'player'){
+        gameBoard.textContent="Well Done you Win!"
     } else{
-        return 0
+        gameBoard.textContent="Boo, you lose!"
     }
 }
 
-function game(){
-    let playerScore = gameRound(getPlayerChoice(), getComputerChoice())
-    playerScore += gameRound(getPlayerChoice(), getComputerChoice())
-    playerScore += gameRound(getPlayerChoice(), getComputerChoice())
-    playerScore += gameRound(getPlayerChoice(), getComputerChoice())
-    playerScore += gameRound(getPlayerChoice(), getComputerChoice())
-
-    if (playerScore > 3) {
-        return 'You Win!'
-    } else{
-        return 'You Lose!'
-    }
-}
-
-console.log(game())
 
